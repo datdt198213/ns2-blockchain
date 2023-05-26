aa0604cfe2166a989a4c4b9ba26601d1f653762a
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2023-05-26 17:52:44 +0700

    modify file name

--- lethanhnghi/control.tcl
+++ lethanhnghi/control.tcl
@@ -13,9 +13,8 @@ set opt(y) 2355;
 set val(stop) 100;
 
 
-#
+
 # Initialize Global Variables
-#
 set ns_		[new Simulator]
 set tracefd     [open lethanhnghi.tr w]
 $ns_ trace-all $tracefd
@@ -28,74 +27,46 @@ set topo       [new Topography]
 
 $topo load_flatgrid $opt(x) $opt(y)
 
-#
+
 # Create God
-#
 create-god $val(nn)
 
-#
+
 #  Create the specified number of mobilenodes [$val(nn)] and "attach" them
 #  to the channel. 
-#  Here two nodes are created : node(0) and node(1)
 
 # configure node
 
-        $ns_ node-config -adhocRouting $val(rp) \
-			 -llType $val(ll) \
-			 -macType $val(mac) \
-			 -ifqType $val(ifq) \
-			 -ifqLen $val(ifqlen) \
-			 -antType $val(ant) \
-			 -propType $val(prop) \
-			 -phyType $val(netif) \
-			 -channelType $val(chan) \
-			 -topoInstance $topo \
-			 -agentTrace ON \
-			 -routerTrace ON \
-			 -macTrace OFF \
-			 -movementTrace ON			
+$ns_ node-config -adhocRouting $val(rp) \
+		-llType $val(ll) \
+		-macType $val(mac) \
+		-ifqType $val(ifq) \
+		-ifqLen $val(ifqlen) \
+		-antType $val(ant) \
+		-propType $val(prop) \
+		-phyType $val(netif) \
+		-channelType $val(chan) \
+		-topoInstance $topo \
+		-agentTrace ON \
+		-routerTrace ON \
+		-macTrace OFF \
+		-movementTrace ON			
 			 
-	for {set i 0} {$i < $val(nn) } {incr i} {
-		set node_($i) [$ns_ node]	
-		$node_($i) random-motion 0		;# disable random motion
-        $ns_ initial_node_pos $node_($i) 20
-	}   
+for {set i 0} {$i < $val(nn) } {incr i} {
+	set node_($i) [$ns_ node]	
+	$node_($i) random-motion 0		;# disable random motion
+	$ns_ initial_node_pos $node_($i) 20
+}   
 
-#
+
 # Provide initial (X,Y, for now Z=0) co-ordinates for mobilenodes
-#
-
-source mobility.tcl
-
+source init.tcl
 
 # Setup traffic flow between nodes
-source cbr48m.tcl
+source traffic.tcl
 
-# UDP connection between node_(0) and node_(1)
-# set udp0 [new Agent/UDP]
-# $ns_ attach-agent $node_(0) $udp0
 
-# set udp1 [new Agent/UDP]
-# $ns_ attach-agent $node_(1) $udp1
-
-# set null0 [new Agent/Null]
-# $ns_ attach-agent $node_(1) $null0
-
-# set null1 [new Agent/Null]
-# $ns_ attach-agent $node_(0) $null1
-
-# $ns_ connect $udp0 $null0
-# $ns_ connect $udp1 $null1
-# $udp0 set fid_ 1
-
-# set cbr [new Application/Traffic/CBR]
-# $cbr attach-agent $udp0
-# $cbr set type_ CBR
-# $cbr set packet_size_ 512
-
-#
 # Tell nodes when the simulation ends
-#
 for {set i 0} {$i < $val(nn) } {incr i} {
     $ns_ at $val(stop) "$node_($i) reset";
 }
